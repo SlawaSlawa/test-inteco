3982731f0a5012b13ae0ca615da654bd351b62c2
Slawa <slawapocht4@yandex.ru>
2021-12-01 15:54:42 +0400

    add sliders

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
 'use strict'
+
+const swiperProjects = new Swiper('.swiper--projects', {
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+
+    navigation: {
+        nextEl: '.slider__arrow-right',
+        prevEl: '.slider__arrow-left',
+    }
+
+});
+
+const swiperWorkWithStaff = new Swiper('.swiper-slider--work-with-staff', {
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.slider__arrow-right',
+        prevEl: '.slider__arrow-left',
+    }
+
+});
+
+const swiperOffersToEmployees = new Swiper('.swiper-slider--offers-to-employees', {
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.slider__arrow-right',
+        prevEl: '.slider__arrow-left',
+    }
+
+});
